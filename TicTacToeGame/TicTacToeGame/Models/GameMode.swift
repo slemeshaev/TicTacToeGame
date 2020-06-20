a928a8a100821467582e42bae10cca7cfa723535
Stanislav <lemeshaevs@gmail.com>
2020-06-20 14:42:42 +0300

    Добавил необходимые файлы и убрал ненужные

---
+++ TicTacToeGame/TicTacToeGame/Models/GameMode.swift
@@ -0,0 +1,9 @@
+//
+//  GameMode.swift
+//  TicTacToeGame
+//
+//  Created by Станислав Лемешаев on 19.06.2020.
+//  Copyright © 2020 Станислав Лемешаев. All rights reserved.
+//
+
+import Foundation
